31137ddd3faccadd62ee00e1c1392f33b1ab3eb7
Nicolas Parra <nicolas.parra0125@jala.university>
2026-05-10 03:50:01 -0500

    Add try catch for args validation

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,61 @@ public class Main {
         // Grid:
         // w= "width", w only allows int and must be either 10, 20 and 40 only
         // h= "height", h only allows int and must be either 10, 20, 40 or 80 only
-        int  widht = Integer.parseInt(args[0].split("=")[1]);
-        int height = Integer.parseInt(args[1].split("=")[1]);
+        int  widht;
+        int height;
 
         // g= "generations", is the number of generations to reproduce, only int values >=0
         // Special value 0=> infinite, it'll stop only if the user does it
-        int generations = Integer.parseInt(args[2].split("=")[1]);
+        int generations;
 
         // s= "speed", int value, only 250 milliseconds or 1000 milliseconds allowed
-        int speed = Integer.parseInt(args[3].split("=")[1]);
+        int speed;
 
         // n= "neighborhood", int [1, 2, 3, 4, 5] default value = 3 which corresponds to a complete neighborhood
-        int neighborhood = Integer.parseInt(args[4].split("=")[1]);
+        String neighborhood;
 
-        // p= "population",
+        // p= "population", String parameter, 000#111#101#010 where 1 is the live cells, 0 is the dead cells and # indicates the separation between them
+        String population;
+
+
+        try {
+            if (args.length < 6) {
+                System.out.println("Incomplete arguments!");
+            }
+
+            for (int i = 1; i < args.length; i++) {
+                if (args[i].split("=")[0].toLowerCase().equals("w")) {
+                    widht = Integer.parseInt(args[i].split("=")[1]);
+                    // System.out.println("widht: " + widht);
+                }
+                else if (args[i].split("=")[0].toLowerCase().equals("h")) {
+                    height = Integer.parseInt(args[i].split("=")[1]);
+                    // System.out.println("height: " + height);
+                }
+                else if (args[i].split("=")[0].toLowerCase().equals("s")) {
+                    speed = Integer.parseInt(args[i].split("=")[1]);
+                    // System.out.println("speed: " + speed);
+                }
+                else if (args[i].split("=")[0].toLowerCase().equals("p")) {
+                    population = args[i].split("=")[1];
+                    // System.out.println("population: " + population);
+                }
+                else if (args[i].split("=")[0].toLowerCase().equals("g")) {
+                    generations = Integer.parseInt(args[i].split("=")[1]);
+                    // System.out.println("generations: " + generations);
+                }
+                else if (args[i].split("=")[0].toLowerCase().equals("n")) {
+                    neighborhood = args[i].split("=")[1];
+                    // System.out.println("neighborhood: " + neighborhood);
+                }
+                else {
+                    System.out.println("Invalid argument!");
+                }
+
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
