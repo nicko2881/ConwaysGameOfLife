2b8c6d70aa45ee8cc25294e08c10c82bbf59fe5a
Nicolas Parra <nicolas.parra0125@jala.university>
2026-05-11 02:27:13 -0500

    Add try catch for args validation

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,28 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         // Parameters:
 
         // Grid:
+
         // w= "width", w only allows int and must be either 10, 20 and 40 only
         // h= "height", h only allows int and must be either 10, 20, 40 or 80 only
-        int  widht;
-        int height;
+        int  widht = 0;
+        int height = 0;
 
         // g= "generations", is the number of generations to reproduce, only int values >=0
         // Special value 0=> infinite, it'll stop only if the user does it
-        int generations;
+        int generations = 0;
 
         // s= "speed", int value, only 250 milliseconds or 1000 milliseconds allowed
-        int speed;
+        int speed = 0;
 
         // n= "neighborhood", int [1, 2, 3, 4, 5] default value = 3 which corresponds to a complete neighborhood
-        String neighborhood;
+        int neighborhood = 0;
 
         // p= "population", String parameter, 000#111#101#010 where 1 is the live cells, 0 is the dead cells and # indicates the separation between them
-        String population;
+        String population = "";
 
 
         try {
@@ -27,7 +30,7 @@ public class Main {
                 System.out.println("Incomplete arguments!");
             }
 
-            for (int i = 1; i < args.length; i++) {
+            for (int i = 0; i < args.length; i++) {
                 if (args[i].split("=")[0].toLowerCase().equals("w")) {
                     widht = Integer.parseInt(args[i].split("=")[1]);
                     // System.out.println("widht: " + widht);
@@ -42,24 +45,36 @@ public class Main {
                 }
                 else if (args[i].split("=")[0].toLowerCase().equals("p")) {
                     population = args[i].split("=")[1];
-                    // System.out.println("population: " + population);
+                    System.out.println("population: " + population);
                 }
                 else if (args[i].split("=")[0].toLowerCase().equals("g")) {
                     generations = Integer.parseInt(args[i].split("=")[1]);
                     // System.out.println("generations: " + generations);
                 }
                 else if (args[i].split("=")[0].toLowerCase().equals("n")) {
-                    neighborhood = args[i].split("=")[1];
+                    neighborhood = Integer.parseInt(args[i].split("=")[1]);
                     // System.out.println("neighborhood: " + neighborhood);
                 }
                 else {
                     System.out.println("Invalid argument!");
                 }
-
             }
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
+            return;
         }
+
+        // Population separation
+        String[] populationRows = population.split("#")[0].split(",");
+        System.out.println("Population: " + populationRows[0]);
+
+        // Grid
+        int[][] board =  new int[widht][height];
+        /* for (int i = 0; i < widht; i++) {
+            for (int j = 0; j < height; j++) {
+                board[i][j] = ;
+            }
+        }*/
     }
 }
